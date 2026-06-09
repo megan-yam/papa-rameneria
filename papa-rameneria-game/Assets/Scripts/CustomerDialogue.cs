9ddf3616a4107520fee8f9693495935037895f66
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2026-06-09 16:53:53 -0700

    dialogue script setup

--- papa-rameneria-game/Assets/Scripts/CustomerDialogue.cs
+++ papa-rameneria-game/Assets/Scripts/CustomerDialogue.cs
@@ -1,18 +1,77 @@
 using UnityEngine;
+using TMPro;
 
 public class CustomerDialogue : MonoBehaviour
 {
     public GameObject speechBubble;
+    public GameObject interactBubble;
+    public TMP_Text dialogueText;
+
     private Customer customer;
+
+    [TextArea]
+    public string greetingDialogue;
+
+    [TextArea]
+    public string orderDialogue;
+
+    public float dialogueDuration = 10f;
+
+    private int dialogueStage = 0;
+
     void Start()
     {
         customer = GetComponent<Customer>();
+
         speechBubble.SetActive(false);
+        interactBubble.SetActive(true);
+    }
+
+    public void StartDialogue()
+    {
+        CancelInvoke(nameof(EndDialogue));
+
+        dialogueStage = 0;
+
+        speechBubble.SetActive(true);
+        interactBubble.SetActive(false);
+
+        dialogueText.text = greetingDialogue;
+        customer.Animator.SetBool("talking", true);
+    }
+
+    public void AdvanceDialogue()
+    {
+        dialogueStage++;
+
+        if (dialogueStage == 1)
+        {
+            dialogueText.text = orderDialogue;
+
+            // Automatically close after 10 seconds
+            Invoke(nameof(EndDialogue), dialogueDuration);
+        }
+    }
+
+    public void EndDialogue()
+    {
+        speechBubble.SetActive(false);
+        interactBubble.SetActive(true);
+
+        customer.Animator.SetBool("talking", false);
+
+        dialogueStage = 0;
     }
 
     void OnMouseDown()
     {
-        speechBubble.SetActive(true);
-        customer.Animator.SetBool("talking", true);
+        if (!speechBubble.activeSelf)
+        {
+            StartDialogue();
+        }
+        else if (dialogueStage == 0)
+        {
+            AdvanceDialogue();
+        }
     }
 }
